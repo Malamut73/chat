9d155b87d89e9a818959b3ebe9b4663af4438a98
Malamut73 <lemoor@mail.ru>
2022-10-18 11:35:03 +0400

    Initial commit

---
+++ src/main/java/com/socket/chat/Greeting.java
@@ -0,0 +1,15 @@
+package com.socket.chat;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Greeting {
+
+    private String message;
+
+}
